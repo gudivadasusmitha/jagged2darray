9a40afeeccf5ea92cb7a4e3ada212f447c405590
gudivadasusmitha <Gudivada.Susmitha@ibm.com>
2022-03-02 15:19:46 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jagged2darray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][,] jaggedArray = new int[3][,]
+            {
+                new int[,] {{1,8},{6,9}},
+                new int[,]{{3,10},{5,6},{9,10}},
+                new int[,]{{11,34},{100,88},{90,10}}
+            };
+            Console.WriteLine(jaggedArray[0][0, 1]);
+            Console.WriteLine(jaggedArray[1][2, 1]);
+            Console.WriteLine(jaggedArray[2][1, 0]);
+            Console.ReadLine();
+        }
+    }
+}
